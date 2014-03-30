f7a9a1a2d0a4089ef6c2879da2955947dff36de6
Matt Hampel <matth@localdata.com>
2014-03-29 22:53:17 -0400

    Working error checker

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 'use strict';
 
 var url = 'http://ddot-beta.herokuapp.com/api/api/where/vehicles-for-agency/DDOT.json?key=BETA';
+var key = "?key=BETA&format=json";
+var base = "http://ddot-beta.herokuapp.com/api/api/where/";
+
 var twilioAccount = process.env.TWILIO || '';
 var twilioAuth = process.env.TWILIO_AUTH || '';
 var phone = process.env.PHONE;
@@ -9,8 +12,15 @@ var phone = process.env.PHONE;
 var http = require('http');
 var _ = require('lodash');
 var moment = require('moment-timezone');
+var async = require('async');
 
-// var client = require('twilio')(twilioAccount, twilioAuth);
+//var client = require('twilio')(twilioAccount, twilioAuth);
+
+/**
+ * This script checks the DDOT API to see how many buses are tracked
+ * If the number isn't reasonable, it sends a text message to the number
+ * listed in the PHONE environment variable.
+ */
 
 function sms(message) {
   client.sms.messages.create({
@@ -29,42 +39,83 @@ if(hour > 0 && hour < 7) {
   process.exit();
 }
 
-// Do the actual checking
+
+// Get all the currently routes
+url = base + "routes-for-agency/DDOT.json" + key;
 http.get(url, function(res) {
   var body = '';
-
   res.on('data', function(chunk) {
     body += chunk;
   });
 
   res.on('end', function() {
-    var content = JSON.parse(body).data;
-    // console.log("Got response: ", content);
+    var data = JSON.parse(body).data;
+    var routes = data.list;
 
-    if (content.list.length < 5) {
-      sms("Suspiciously few buses on the street");
-    }
+    var counts = {
+      total: 0,
+      ontime: 0,
+      behind: 0,
+      late: 0,
+      verylate: 0,
+      unknown: 0
+    };
 
-    // See how many buses are tracked
-    // This is WRONG!!
-    var out = 0;
-    var untracked = 0;
-    _.each(content.list, function(elt) {
-        out += 1;
+    // For each route, get the list of active trips
+    async.each(routes, function(route, done){
+      url = base + "trips-for-route/" + route.id + ".json" + key + "&includeStatus=true";
+      http.get(url, function(res) {
+        var raw = '';
+        res.on('data', function(chunk) {
+          raw += chunk;
+        });
 
-        console.log(elt);
+        res.on('end', function() {
+          // Get the list of trips
+          var trips = JSON.parse(raw).data;
+          var listOfTrips = trips.list;
 
-        if (elt.tripStatus && !elt.tripStatus.predicted) {
-          untracked += 1;
-        }
-    });
+          // Check the deviation for each trip
+          _.each(listOfTrips, function(trip){
+            var deviation = trip.status.scheduleDeviation / 60;
+            var absDeviation = Math.abs(deviation);
+            var severity;
+            if (absDeviation >= 0) { severity = "ontime"; }
+            if (absDeviation >= 5) { severity = "behind"; }
+            if (absDeviation >= 10) { severity = "late"; }
+            if (absDeviation >= 20) { severity ="verylate"; }
 
-    console.log(out, untracked);
-    if (untracked > (out/2)) {
-      sms("Less than half of the buses are tracked");
-    }
+            if (trip.status.predicted === false) {
+              deviation = "?";
+              severity = "unknown";
+            }
+            counts[severity] += 1;
+            counts.total += 1;
+          });
+
+          done();
+        });
+      }); // end route query
+    }, function(err) {
+      // Done checking each route.
+      console.log("Counts:", counts);
+
+      // If there are fewer than 5 buses on the road, something is wrong.
+      if (counts.total < 5) {
+        console.log("WARNING: Suspiciously few buses are tracked");
+        sms("WARNING: Suspiciously few buses on the street.");
+      }
+
+      // If more than half the buses are untracked, there is a problem
+      if (counts.unknown > (counts.total/2)) {
+        console.log("WARNING: Less than half of the buses are tracked");
+        sms("WARNING: Less than half of the buses are tracked");
+      }
+
+    }.bind(this));
+  }).on('error', function(error) {
+    // If we got an error from the API call
+    console.log("API error: ", error);
+    sms("ERROR: TMB API returned " + error);
   });
-
-}).on('error', function(e) {
-  sms("TMB error " + e.message);
 });
